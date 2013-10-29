9ab130e8e13d6dff243b3558bd1713eef2b62e52
zmmbreeze <zmmbreeze0825@gmail.com>
2013-10-29 08:50:44 +0800

    Inin

---
+++ sidebar.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The Sidebar containing the main widget areas.
+ *
+ * @package Toolbox
+ * @since Toolbox 0.1
+ */
+?>
+    <div class="side" role="complementary">
+        <?php if (is_single() || is_tag() || !is_category() || is_page()) { ?>
+        <header class="header" role="banner">
+            <hgroup>
+                <h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+                <p><?php bloginfo( 'description' ); ?></p>
+            </hgroup>
+        </header><!-- #branding -->
+        <?php } ?>
+
+        <aside id="octocard"></aside>
+        <script>
+        OCTOCARD = {
+          name: 'zmmbreeze',
+          modules: 'base,repos,eventsStatis,orgs'
+        };
+        </script>
+        <script src="http://nodejs.in/octocard/bin/octocard.js"></script>
+        <aside>
+
+        <aside class="side-rss">
+            <a href="<?php bloginfo( 'rss2_url' ); ?>">RSS</a>
+        </aside>
+
+        <?php wp_list_bookmarks('category_before=<aside>&category_after=</aside>'); ?> 
+    </div>
