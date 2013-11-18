c4edd9b6213366c72f4e6e6f57f547a8009ac21a
zmmbreeze <zmmbreeze0825@gmail.com>
2013-11-18 12:43:48 +0800

    use Lora font

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 
     ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Libre+Baskerville" />
+<link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet'>
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
